8ffa328c7d067a543bd2686c0157ac56a5997dab
emilycavalcanti <emilycalvacanti.s@gmail.com>
2024-04-11 15:15:24 -0300

    resolucao exercicio votacao

--- src/implementacao/logica/votacao.ts
+++ src/implementacao/logica/votacao.ts
@@ -1,4 +1,32 @@
 function apuraVotacao(votos: Array<number>) {
-    return null;
+
+    let contador1=0;
+    let contador2=0;
+    let contador3=0;
+
+    for(let i=0; i< votos.length; i++)
+        if(votos[i] === 1){
+            contador1++
+        }
+        else if (votos[i] === 2){
+            contador2++
+        }
+        else if (votos[i] === 3){
+            contador3++
+        } 
+    if(contador1 > contador2 && contador1 > contador3){
+        return ("Vencedor: 1")
+    }
+    
+   else if(contador2 > contador1 && contador2 > contador3){
+        return ("Vencedor: 2")
+    } 
+
+    else if(contador3 > contador1 && contador3>contador2){
+        return ("Vencedor: 3")
+    }    
+    else {
+        return ("Empate")
+    }    
 }
 module.exports = apuraVotacao;
