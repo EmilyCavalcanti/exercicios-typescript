b5494bb7406d62f0332886b4aee2b94b543fe5f9
Vivian <viviancristinavidal@gmail.com>
2024-04-11 14:10:48 -0300

    resolução exercício contadorDeOcorrencia

--- src/implementacao/logica/contador-de-ocorrencia.ts
+++ src/implementacao/logica/contador-de-ocorrencia.ts
@@ -1,4 +1,12 @@
 function contadorDeOcorrencia(texto: string, letra: string) {
-    return null;
+    
+    let contador = 0;
+    for (let i =0; i <texto.length; i++){
+        let palavra = letra;
+        if (texto[i] == palavra ){
+            contador++
+        }}
+        return contador;
+        
 }
 module.exports = contadorDeOcorrencia;
