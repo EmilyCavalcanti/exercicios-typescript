1fbadb2c993ab6e0e2abe659ed049c82ae225362
Vivian <viviancristinavidal@gmail.com>
2024-04-12 14:54:47 -0300

    Resolução armstrong

--- src/implementacao/logica/numeros-armstrong.ts
+++ src/implementacao/logica/numeros-armstrong.ts
@@ -1,4 +1,19 @@
 function numerosArmstrong(numero: number) {
-    return null;
+    let digitos = numero.toString();
+    let resultado = 0;
+    let resultado1 =0 ;
+    
+    for(let i =0; i < digitos.length; i++){
+        let numeral = parseInt(digitos.charAt(i))
+        resultado = numeral**digitos.length;
+        resultado1+=resultado;
+    }
+ 
+    if (resultado1 === numero){
+        return("Este eh um numero de Armstrong!")
+    } 
+    else{
+        return("Este nao eh um numero de Armstrong!")
+    }
 }
 module.exports = numerosArmstrong;
